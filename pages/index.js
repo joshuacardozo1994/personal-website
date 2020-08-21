3ea5a39600fe8a728f59ef04c1f9b56f241f69e7
Joshua Cardozo <joshua.cardozo@bidchat.com>
2020-08-21 13:34:51 +0530

    dynamic text and background

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-    <h1>Coming Soon</h1>
-    </div>
+    <h1 className="dynamic-text">Coming Soon</h1>
   )
 }
