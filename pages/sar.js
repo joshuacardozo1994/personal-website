48620933bd3bfed7d64f5d5b2be07c93ed5e3985
Joshua Cardozo <joshua.cardozo@bidchat.com>
2020-08-29 01:44:00 +0530

    test page

---
+++ pages/sar.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+const SAR = () => {
+    return (
+        <h1 className="dynamic-text" style={{ textAlign: 'center', marginTop: 50, minHeight: '100vh' }}>Actually your ass is sexier</h1>
+    )
+}
+
+export default SAR;
