2b694945c41b18bc92a35f15ab702488b7660e10
Joshua Cardozo <joshua.cardozo@bidchat.com>
2020-08-29 02:00:10 +0530

    test

--- pages/sar.js
+++ pages/sar.js
@@ -1,8 +1,21 @@
-import React from 'react' 
+import React, { useState, useEffect } from 'react'
 
 const SAR = () => {
+    const [showSender, setShowSender] = useState(false);
+
+    useEffect(() => {
+		const init = () => {
+			setTimeout(() => {
+                setShowSender(true)
+            }, 10000);
+		};
+		init();
+	  }, []);
     return (
-        <h1 className="dynamic-text" style={{ textAlign: 'center', marginTop: 50, minHeight: '100vh' }}>Actually your ass is sexier</h1>
+        <div style={{ minHeight: '100vh' }} >
+        <h1 className="dynamic-text" style={{ textAlign: 'center', marginTop: 50 }}>Actually your ass is sexier</h1>
+        <div style={{ display: "flex", justifyContent: 'flex-end', marginRight: 50 }} >{showSender ? "-Rochelle to Joshua" : ""}</div>
+        </div>
     )
 }
 
