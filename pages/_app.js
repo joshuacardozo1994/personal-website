7a0679adc5b9a1feaf3836b68617a683bd3a0205
Joshua Cardozo <joshua.cardozo@bidchat.com>
2020-08-28 22:43:55 +0530

    added social meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,11 +9,15 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Joshua Cardozo - Developer</title>
-        <meta name="description" content="Mobile and front-end Developer" />
+        <title>Joshua Cardozo</title>
+        <meta name="description" content="Mobile and front-end Developer based in Goa, India" />
         <meta name="keywords" content="Swift, iOS, JavaScript, React, NextJS, HTML, CSS" />
         <meta name="author" content="Joshua Cardozo" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Joshua Cardozo" />
+        <meta property="og:description" content="Mobile and front-end Developer based in Goa, India" />
+        <meta property="og:image" content="https://joshuacardozo.com/profile.jpg" />
+        <meta property="og:url" content="https://joshuacardozo.com/joshua-banner.jpg" />
       </Head>
       <Header />
       <Component {...pageProps}/>
