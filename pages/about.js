ce491cdf81f922feb5e4dc790a685bd67ee400ad
Joshua Cardozo <joshua.cardozo@bidchat.com>
2020-09-09 02:43:51 +0530

    added plain pages for non home pages

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => (
+    <div style={{ minHeight: '100vh' }}>
+    <h1 className="dynamic-text text-alin-center mt-15">Coming Soon</h1>
+    </div>
+)
+
+export default About;
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Contact = () => (
+    <div style={{ minHeight: '100vh' }}>
+    <h1 className="dynamic-text text-alin-center mt-15">Coming Soon</h1>
+    </div>
+)
+
+export default Contact;
