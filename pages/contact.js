b49503a31c600d0a532acb32fd99338ff02adf76
Joshua <joshua.cardozo@gmail.com>
2020-10-03 02:00:07 +0530

    contact page scaffolding done

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const Contact = () => (
-    <div style={{ minHeight: '100vh' }}>
-    <h1 className="dynamic-text text-align-center mt-15">Coming Soon</h1>
+    <div className="container" >
+    <div className="max-readable-width" style={{ margin: 'auto', display: 'flex', flexDirection: 'column', minHeight: 'calc(100vh - 285px)' }} >
+        <h1 className="dynamic-text mt-15" style={{ color: '#ff6961' }}>Don't be a stranger</h1>
+        <h1 className="dynamic-text mt-15">say hello.</h1>
+        <p className="dynamic-text mt-30 max-readable-width" >I'm not currently looking for any new opportunities, but feel free to get in touch, if you need any assistance related to the skills I possess.</p>
+        <address>
+            <span for="email" className="dynamic-text" style={{ display: 'block', marginTop: '2em', opacity: 0.5 }}>Need help?</span>
+            <a id="email" href="mailto:joshua.cardozo@gmail.com">joshua.cardozo@gmail.com</a>
+            <span className="dynamic-text" style={{ display: 'block', marginTop: '2em', opacity: 0.5 }}>Feel like talking</span>
+            <a href="tel:+918806728818">+91 8806728818</a>
+        </address>
+    </div>
     </div>
 )
 
