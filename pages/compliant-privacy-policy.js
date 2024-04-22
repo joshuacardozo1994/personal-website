d8fa7025c98d12d95a1abdb93ec2fac7651e7d77
joshuacardozo1994 <22386718+joshuacardozo1994@users.noreply.github.com>
2024-04-22 15:10:48 +0530

    a compliant privacy policy added

---
+++ pages/compliant-privacy-policy.js
@@ -0,0 +1,31 @@
+export default function PrivacyPolicy() {
+    return (
+        <>
+            <h1>Privacy Policy</h1>
+            <p>Effective date: April 22, 2024</p>
+
+            <h2>Introduction</h2>
+            <p>At [Company Name], we are committed to maintaining the trust and confidence of our visitors to our web site. In particular, we want you to know that [Company Name] is not in the business of selling, renting or trading email lists with other companies and businesses for marketing purposes. This Privacy Policy provides detailed information on when and why we collect your personal information, how we use it, the limited conditions under which we may disclose it to others, and how we keep it secure.</p>
+
+            <h2>Information We Collect</h2>
+            <p>As part of the registration process for our monthly e-newsletter, we collect personal information. We use that information for a couple of reasons: to tell you about stuff you’ve asked us to tell you about; to contact you if we need to obtain or provide additional information; to check our records are right and to check every now and then that you’re happy and satisfied.</p>
+
+            <h2>Lawful Basis for Processing</h2>
+            <p>We process your information under the following:</p>
+            <ul>
+                <li>Consent: You have given your consent for processing personal data for one or more specific purposes.</li>
+                <li>Performance of a contract: Provision of Personal Data is necessary for the performance of an agreement with you and/or for any pre-contractual obligations thereof.</li>
+                <li>Legal obligations: Processing Personal Data is necessary for compliance with a legal obligation to which the company is subject.</li>
+            </ul>
+
+            <h2>Your Rights Under GDPR</h2>
+            <p>Under GDPR, you are entitled to view, amend, or delete the personal information that we hold. Email your request to our data protection officer at [DPO Email] and we will work with you to comply with your request.</p>
+
+            <h2>Changes to this Privacy Policy</h2>
+            <p>The privacy policy may change from time to time in line with legislation or industry developments. We will not explicitly inform our clients or website users of these changes. Instead, we recommend that you check this page occasionally for any policy changes.</p>
+
+            <h2>Contact Us</h2>
+            <p>If you have any questions regarding this policy or your dealings with our website, please contact us here: [Contact Information]</p>
+        </>
+    )
+}
